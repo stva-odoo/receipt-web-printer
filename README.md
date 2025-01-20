11be989bc0f7b5e1dde5ad015e2d42cfa98d5a9f
Stephane Vanmeerhaeghe (stva) <stva@odoo.com>
2025-01-20 10:56:00 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,27 @@
+# Receipt Printer Emulator
+
+A simple project to emulate a receipt printer by displaying the receipt on a web page. This is ideal for testing and demonstrating receipt layouts without the need for a physical printer.
+
+---
+
+## Installation
+
+1. Clone the repository:
+   ```bash
+   git clone https://github.com/stva-odoo/receipt-web-printer/
+   cd receipt-web-printer
+   ```
+2. Install dependencies:
+   ```bash
+   npm install
+   ```
+3. Start the application
+   ```bash
+   npm start
+   ```
+4. Open your web browser and navigate to:
+   http://localhost:3000
+
+## Configuration
+
+In the printer settings, set the Epson printer’s IP address to `127.0.0.1:3000`
---
+++ index.js
@@ -0,0 +1,42 @@
+const express = require("express");
+const http = require("http");
+const { Server } = require("socket.io");
+var cors = require("cors");
+const path = require("path");
+const { convertRasterFile } = require("./lib/epson");
+const os = require("os");
+const fs = require("fs-extra");
+
+const app = express();
+const server = http.createServer(app);
+const io = new Server(server);
+const receiptDir = path.join(os.tmpdir(), "pos_receipt/");
+fs.emptyDirSync(receiptDir);
+
+app.use(cors());
+app.use(express.static(path.join(__dirname, "public")));
+app.use("/receipt", express.static(receiptDir));
+app.use(
+  express.raw({
+    type: ["image/*", "text/plain"],
+    limit: "50mb",
+  })
+);
+
+app.post("/cgi-bin/epos/service.cgi", async (req, res) => {
+  const filename = `r-${Date.now()}.jpg`;
+  const destFilePath = path.join(receiptDir, filename);
+  try {
+    await convertRasterFile(req.body, destFilePath);
+    res.send("<response success='true'></response>");
+  } catch (err) {
+    console.error("Error converting data:", err);
+    return res.sendStatus(500);
+  }
+  io.emit("newImage", { filename: "receipt/" + filename });
+});
+
+const PORT = process.env.PORT || 3000;
+server.listen(PORT, () => {
+  console.log(`Server listening on port ${PORT}`);
+});
